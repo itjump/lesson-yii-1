4bd91507b4326cbf104360f6d606af7cd73a1b4c
Almaz Mubinov <almaz.mubinov@gmail.com>
2015-03-14 21:17:13 +0300

    MySQL dumo

--- protected/data/schema.mysql.sql
+++ protected/data/schema.mysql.sql
@@ -1,28 +1,67 @@
-CREATE TABLE tbl_user (
-    id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    username VARCHAR(128) NOT NULL,
-    password VARCHAR(128) NOT NULL,
-    email VARCHAR(128) NOT NULL
-);
+--
+-- Отключение внешних ключей
+-- 
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 
-INSERT INTO tbl_user (username, password, email) VALUES ('test1', 'pass1', 'test1@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test2', 'pass2', 'test2@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test3', 'pass3', 'test3@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test4', 'pass4', 'test4@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test5', 'pass5', 'test5@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test6', 'pass6', 'test6@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test7', 'pass7', 'test7@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test8', 'pass8', 'test8@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test9', 'pass9', 'test9@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test10', 'pass10', 'test10@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test11', 'pass11', 'test11@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test12', 'pass12', 'test12@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test13', 'pass13', 'test13@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test14', 'pass14', 'test14@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test15', 'pass15', 'test15@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test16', 'pass16', 'test16@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test17', 'pass17', 'test17@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test18', 'pass18', 'test18@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test19', 'pass19', 'test19@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test20', 'pass20', 'test20@example.com');
-INSERT INTO tbl_user (username, password, email) VALUES ('test21', 'pass21', 'test21@example.com');
+-- 
+-- Установить режим SQL (SQL mode)
+-- 
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+-- 
+-- Установка кодировки, с использованием которой клиент будет посылать запросы на сервер
+--
+SET NAMES 'utf8';
+
+--
+-- Описание для таблицы expert
+--
+CREATE TABLE IF NOT EXISTS expert (
+  id INT(11) NOT NULL AUTO_INCREMENT,
+  name VARCHAR(50) DEFAULT NULL COMMENT 'Имя',
+  avatar VARCHAR(255) DEFAULT NULL COMMENT 'Аватар',
+  rating INT(11) DEFAULT NULL COMMENT 'Рейтинг',
+  status ENUM('expert') DEFAULT 'expert' COMMENT 'Статус',
+  skills VARCHAR(1000) DEFAULT NULL COMMENT 'Интересы',
+  description VARCHAR(1000) DEFAULT NULL COMMENT 'Описание',
+  deleted TINYINT(1) DEFAULT 0,
+  PRIMARY KEY (id)
+)
+ENGINE = INNODB
+CHARACTER SET utf8
+COLLATE utf8_general_ci;
+
+--
+-- Описание для таблицы user
+--
+CREATE TABLE IF NOT EXISTS user (
+  id INT(11) NOT NULL AUTO_INCREMENT,
+  username VARCHAR(255) DEFAULT NULL COMMENT 'Логин',
+  password VARCHAR(255) DEFAULT NULL COMMENT 'Пароль',
+  email VARCHAR(50) DEFAULT NULL COMMENT 'Email',
+  date TIMESTAMP NULL DEFAULT NULL COMMENT 'Дата создания',
+  name VARCHAR(50) DEFAULT NULL COMMENT 'Имя',
+  role ENUM('admin','user') DEFAULT NULL COMMENT 'Роль',
+  deleted TINYINT(1) DEFAULT 0,
+  PRIMARY KEY (id)
+)
+ENGINE = INNODB
+CHARACTER SET utf8
+COLLATE utf8_general_ci;
+
+-- 
+-- Вывод данных для таблицы user
+--
+INSERT INTO user VALUES
+(1, 'test', 'test', 'test@mail.ru', '0000-00-00 00:00:00', 'Test Testov', 'user', 0),
+(3, 'admin', 'admin', 'admin@mail.ru', NULL, 'Admin', 'admin', 0);
+
+-- 
+-- Восстановить предыдущий режим SQL (SQL mode)
+-- 
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+
+-- 
+-- Включение внешних ключей
+-- 
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
